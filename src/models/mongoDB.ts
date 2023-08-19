f263e261da44daedf5f8b09ac7a4234a6dc200d1
Andra <andradwikasa@gmail.com>
2023-08-19 23:49:45 +0700

    Create database module

---
+++ src/models/mongoDB.ts
@@ -0,0 +1,15 @@
+import { MongoClient, Db } from "mongodb";
+
+const uri = "mongodb+srv://andradwikasa:3bZ8AFzRt5Sk1T6K@cluster0.wowqlzw.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+const db = 'RevoU';
+const pass = "3bZ8AFzRt5Sk1T6K"
+
+async function connectToDatabase(): Promise<Db> {
+    await client.connect();
+    console.log("Berhasil connect")
+    return client.db(db);
+}
+
+export default connectToDatabase;
+
