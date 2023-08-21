17246f8966cc8ba1dacaff5de6a0857422b02335
Andra <andradwikasa@gmail.com>
2023-08-21 11:53:39 +0700

    Create user validation response

---
+++ src/utils/userValidationResponse.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userValidation = void 0;
+exports.userValidation = {
+    userNameExist: "Username already exist",
+    userIsBlank: "Username can't be blank",
+    passwordMinLength: "Password min length is 8",
+    passwordNoNumber: "Password must be one number at least",
+    passwordNoLetter: "Password must be one letter at least"
+};
---
+++ src/utils/userValidationResponse.ts
@@ -0,0 +1,8 @@
+export const userValidation = {
+    userNameExist: "Username already exist",
+    userIsBlank: "Username can't be blank",
+    passwordMinLength: "Password min length is 8",
+    passwordNoNumber: "Password must be one number at least",
+    passwordNoLetter: "Password must be one letter at least"
+}
+
