818ca834a853272e1d2ec4d4c39d53a662af577f
Andra <andradwikasa@gmail.com>
2023-08-22 12:01:35 +0700

    Add transfer model for mongoDB

---
+++ src/models/transfers.js
@@ -0,0 +1,40 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createTransfers = exports.getAllTranfers = void 0;
+const mongoDB_1 = __importDefault(require("../config/mongoDB"));
+function getAllTranfers() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const db = yield (0, mongoDB_1.default)();
+        const transferCollection = db.collection('transfers');
+        const transfers = yield transferCollection.find().toArray();
+        return transfers;
+    });
+}
+exports.getAllTranfers = getAllTranfers;
+function createTransfers(newTransfer) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const db = yield (0, mongoDB_1.default)();
+        const transferCollection = db.collection('transfers');
+        const newTransferAdd = yield transferCollection.insertOne(newTransfer);
+        return newTransferAdd;
+    });
+}
+exports.createTransfers = createTransfers;
+// export async function getUserByUsername(userName: string) {
+//     const db = await connectToDatabase();
+//     const userCollection = db.collection('users');
+//     const userResult = await userCollection.findOne({username: userName});
+//     return userResult   
+// }
---
+++ src/models/transfers.ts
@@ -0,0 +1,31 @@
+import connectToDatabase from "../config/mongoDB";
+
+interface TransferModel {
+    fromAccountId: string;
+    toAccountId: string;
+    amount: number;
+    status: string;
+    timestamp: Date;
+}
+
+export async function getAllTranfers() {
+    const db = await connectToDatabase();
+    const transferCollection = db.collection('transfers');
+    const transfers = await transferCollection.find().toArray();
+    return transfers
+}
+
+export async function createTransfers(newTransfer: TransferModel) {
+    const db = await connectToDatabase();
+    const transferCollection = db.collection('transfers');
+    const newTransferAdd = await transferCollection.insertOne(newTransfer);
+    return newTransferAdd
+}
+
+// export async function getUserByUsername(userName: string) {
+//     const db = await connectToDatabase();
+//     const userCollection = db.collection('users');
+//     const userResult = await userCollection.findOne({username: userName});
+//     return userResult   
+// }
+
