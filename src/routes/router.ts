ab221b8b610eb0457f1757814d8d8b426eefd3d2
Andra <andradwikasa@gmail.com>
2023-08-24 02:33:19 +0700

    Setup swagger

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,11 +1,21 @@
 import { Router } from "express";
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from "swagger-jsdoc";
 import { createUserController, getUsersController, loginUser } from "../controllers/userController";
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
+
+const yamlContent = fs.readFileSync('doc/apiDoc.yaml', 'utf8');
+const swaggerDocument: any = yaml.load(yamlContent)
 
 const router = Router()
 
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
 router.get('/users', getUsersController);
 
-router.post('/users', createUserController)
+router.post('/create', createUserController)
 
-router.post('/users/login', loginUser)
+router.post('/login', loginUser)
 export default router;
