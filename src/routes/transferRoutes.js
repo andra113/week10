bd6d4d20d62fcd62c7184706a6fe1e9ba6186e39
Andra <andradwikasa@gmail.com>
2023-08-23 09:52:19 +0700

    Refactor indentation and whitespaces, and add import

--- src/routes/transferRoutes.js
+++ src/routes/transferRoutes.js
@@ -1,8 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const transferController_1 = require("../controllers/transferController");
+const jwtAuth_1 = __importDefault(require("../middleware/jwtAuth"));
 const transferRouter = (0, express_1.Router)();
-transferRouter.get('/transfers', transferController_1.getTransfersController);
+transferRouter.get('/transfers', jwtAuth_1.default, transferController_1.getTransfersController);
 transferRouter.post('/transfers', transferController_1.createTransfersController);
 exports.default = transferRouter;
--- src/routes/transferRoutes.ts
+++ src/routes/transferRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { getTransfersController, createTransfersController } from "../controllers/transferController";
+import authentication from "../middleware/jwtAuth";
 
 const transferRouter = Router();
 
-transferRouter.get('/transfers', getTransfersController);
+transferRouter.get('/transfers', authentication, getTransfersController);
+
 transferRouter.post('/transfers', createTransfersController);
 
 export default transferRouter
